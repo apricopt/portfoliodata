3f965f85571059f360942e7ac103a8e928ac784b
zohaib <apricopt@gmail.com>
2020-08-13 09:11:14 +0500

    first

---
+++ Localdata/commercialprojects.js
@@ -0,0 +1,97 @@
+const Data = 
+{
+
+  // aljo project starts
+   "TOSHIBA" :{
+
+    "headertitle" : "TOSHIBA PROJECT",
+    "header":require('../HOME PAGE/interior/commercial/aljo/aljoheader.jpg'),
+    "headerMobile":require('../HOME PAGE/interior/commercial/aljo/headermobilealjo.jpg'),
+    "headerLocation" : "Jeddah, KSA",
+
+
+    "text1" : "A complete renovation project, Designed by Rawan AlSahsah, her villa is the essence of what RIS design is. With her extremely exquisite taste in just about everything, every little detail from the floor to the ceiling was hand picked and customized. By mixing different styles and materials, she was able to create a space that is very subtle yet keeps volume about her abilities. to completely re-design the interior of the villa, an entirely new concept was implemented. Mixing of different styles helped create spaces that were bold and chic, the mix of colors brought a sense of freshness and femininity.",
+
+    "img1": require("../HOME PAGE/interior/commercial/aljo/secpicsizealjo.jpg"),
+
+    "slider1" :require("../HOME PAGE/interior/commercial/aljo/slider1aljo.jpg"),
+    "slider2" :require("../HOME PAGE/interior/commercial/aljo/slider2aljo.jpg"),
+    "slider3" :require("../HOME PAGE/interior/commercial/aljo/slider3aljo.jpg"),
+
+
+
+    "img2": require("../HOME PAGE/interior/commercial/aljo/singlepicaljo.jpg"),
+    "text2" : "A complete renovation project, Designed by Rawan AlSahsah, her villa is the essence of what RIS design is. With her extremely exquisite taste in just about everything, every little detail from the floor to the ceiling was hand picked and customized. By mixing different styles and materials, she was able to create a space that is very subtle yet keeps volume about her abilities. to completely re-design the interior of the villa, an entirely new concept was implemented. Mixing of different styles helped create spaces that were bold and chic, the mix of colors brought a sense of freshness and femininity.",
+   
+    "twoCardsImage": require("../HOME PAGE/interior/commercial/aljo/collage2aljo.jpg"),
+  
+    "threeCardsImage": require("../HOME PAGE/interior/commercial/aljo/collage3aljo.jpg"),
+  
+   
+  },
+  // aljo project ends
+
+  // mofa project starts
+  "MOFA" :{
+
+    "headertitle" : "MOFA",
+    "header":require('../HOME PAGE/interior/commercial/mofa/headermofa.jpg'),
+    "headerMobile":require('../HOME PAGE/interior/commercial/mofa/headermobilemofa.jpg'),
+    "headerLocation" : "Jeddah, KSA",
+
+
+    "text1" : "A complete renovation project, Designed by Rawan AlSahsah, her villa is the essence of what RIS design is. With her extremely exquisite taste in just about everything, every little detail from the floor to the ceiling was hand picked and customized. By mixing different styles and materials, she was able to create a space that is very subtle yet keeps volume about her abilities. to completely re-design the interior of the villa, an entirely new concept was implemented. Mixing of different styles helped create spaces that were bold and chic, the mix of colors brought a sense of freshness and femininity.",
+
+    "img1": require("../HOME PAGE/interior/commercial/mofa/secpicsizemofa.jpg"),
+
+    "slider1" :require("../HOME PAGE/interior/commercial/mofa/slider1mofa.jpg"),
+    "slider2" :require("../HOME PAGE/interior/commercial/mofa/slider2mofa.jpg"),
+    "slider3" :require("../HOME PAGE/interior/commercial/mofa/slider3mofa.jpg"),
+
+
+
+    "img2": require("../HOME PAGE/interior/commercial/mofa/singlepicmofa.jpg"),
+    "text2" : "A complete renovation project, Designed by Rawan AlSahsah, her villa is the essence of what RIS design is. With her extremely exquisite taste in just about everything, every little detail from the floor to the ceiling was hand picked and customized. By mixing different styles and materials, she was able to create a space that is very subtle yet keeps volume about her abilities. to completely re-design the interior of the villa, an entirely new concept was implemented. Mixing of different styles helped create spaces that were bold and chic, the mix of colors brought a sense of freshness and femininity.",
+   
+    "twoCardsImage": require("../HOME PAGE/interior/commercial/mofa/collage2mofa.jpg"),
+  
+    "threeCardsImage": require("../HOME PAGE/interior/commercial/mofa/collage3mofa.jpg"),
+  
+   
+  },
+  // mofa project ends
+// mdc project starts
+"MDC" :{
+
+  "headertitle" : "MDC",
+  "header":require('../HOME PAGE/interior/commercial/mdc/mdcheader.jpg'),
+  "headerMobile":require('../HOME PAGE/interior/commercial/mdc/headermobilemdc.jpg'),
+  "headerLocation" : "Jeddah, KSA",
+
+
+  "text1" : "A complete renovation project, Designed by Rawan AlSahsah, her villa is the essence of what RIS design is. With her extremely exquisite taste in just about everything, every little detail from the floor to the ceiling was hand picked and customized. By mixing different styles and materials, she was able to create a space that is very subtle yet keeps volume about her abilities. to completely re-design the interior of the villa, an entirely new concept was implemented. Mixing of different styles helped create spaces that were bold and chic, the mix of colors brought a sense of freshness and femininity.",
+
+  "img1": require("../HOME PAGE/interior/commercial/mdc/secpicsizemdc.jpg"),
+
+  "slider1" :require("../HOME PAGE/interior/commercial/mdc/slider1mdc.jpg"),
+  "slider2" :require("../HOME PAGE/interior/commercial/mdc/slider2mdc.jpg"),
+  "slider3" :require("../HOME PAGE/interior/commercial/mdc/slider3mdc.jpg"),
+
+
+
+  "img2": require("../HOME PAGE/interior/commercial/mdc/singlepicmdc.jpg"),
+  "text2" : "A complete renovation project, Designed by Rawan AlSahsah, her villa is the essence of what RIS design is. With her extremely exquisite taste in just about everything, every little detail from the floor to the ceiling was hand picked and customized. By mixing different styles and materials, she was able to create a space that is very subtle yet keeps volume about her abilities. to completely re-design the interior of the villa, an entirely new concept was implemented. Mixing of different styles helped create spaces that were bold and chic, the mix of colors brought a sense of freshness and femininity.",
+ 
+  "twoCardsImage": require("../HOME PAGE/interior/commercial/mdc/collage2mdc.jpg"),
+
+  "threeCardsImage": require("../HOME PAGE/interior/commercial/mdc/collage3mdc.jpg"),
+
+ 
+},
+// mdc project ends
+
+}
+
+
+
+export default Data
